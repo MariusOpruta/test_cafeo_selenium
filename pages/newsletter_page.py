53113a358332f49d5198d599587bc047d4f9c0e8
MariusOpruta <opmasorin@gmail.com>
2023-02-07 14:07:08 +0200

    test newsleter new

--- pages/newsletter_page.py
+++ pages/newsletter_page.py
@@ -1,19 +1,11 @@
-
-
-from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
-
 
 
 class NewsletterPage:
     BUTTON_NEWSLETTER = (By.CSS_SELECTOR,'[class="button btn btn-add btn-newsletter"]')
+    #IFRAME = (By.CSS_SELECTOR, "iframe[id^='movalio-email-widget']")
     EMAIL_INPUT = (By.ID,"input-11")
-
-    #FORM = (By.ID,"movalio-email-widget-1675687826213-327-12")
-    FORM = (By.CLASS_NAME,"desktop visible relative email-signup-container")
-    #FORM = (By.CSS_SELECTOR,'[class="desktop visible relative email-signup-container"]')
-
-    SELECT_TERMS = (By.CLASS_NAME,"col col-1 align-self-start")
+    SELECT_TERMS = (By.CSS_SELECTOR, "div.v-input--selection-controls__input")
     LINK_TERMS = (By.CSS_SELECTOR,'[href="https://cafeo.ro/p/termeni"]')
     PAGE_TERMS = (By.CSS_SELECTOR,'[class="breadcrumb clearfix"]')  # Termeni și condiții de utilizare
     LINK_POLITICS = (By.CSS_SELECTOR,'[href="https://cafeo.ro/p/privacy"]')
@@ -26,16 +18,11 @@ class NewsletterPage:
     def button_newsletter(self):
         self.browser.find_element(*self.BUTTON_NEWSLETTER).click()
     def switch(self):
-        #self.browser.switch_to.frame('movalio-email-widget-1675687826213-327-12')
-
-        # iframe = self.browser.find_element(By.ID,"movalio-email-widget-1675752669411-296-12")
-        # self.browser.switch_to.frame(iframe)
-
-        # self.browser.switch_to.frame('movalio-email-widget-1675687826213-327-12')
-
-        iframe = self.browser.find_element(By.CSS_SELECTOR,"#overlay > iframe")
+        iframe = self.browser.find_element(By.CSS_SELECTOR, "iframe[id^='movalio-email-widget']")
         self.browser.switch_to.frame(iframe)
-
+        # #self.browser.find_element(self.EMAIL_INPUT).send_keys("email@yahoo.com")
+        # self.browser.find_element(*self.SELECT_TERMS).click()
+        # self.browser.find_element(*self.LINK_TERMS).click()
     def email_input(self,email):
         self.browser.find_element(self.EMAIL_INPUT).send_keys(email)
 
@@ -47,15 +34,16 @@ class NewsletterPage:
 
     def get_page_terms(self):
         self.browser.switch_to(*self.LINK_TERMS)
-        return self.browser.find_element(*self.PAGE_TERMS).text
+        #return self.browser.find_element(*self.PAGE_TERMS).text
+        self.browser.switch_to.default_content()
 
     def click_link_politics(self):
         self.browser.find_element(*self.LINK_POLITICS).click()
 
     def get_page_politics(self):
         self.browser.switch_to(*self.LINK_POLITICS)
-        return self.browser.find_element(*self.PAGE_POLITICS).text
-
+#        return self.browser.find_element(*self.PAGE_POLITICS).text
+        self.browser.switch_to.default_content()
 
     def click_buton_ok(self):
         self.browser.find_element(*self.BUTTON_OK).click()
